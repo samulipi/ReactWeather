79996a024161fe377f3749776715d9e07da05da1
Ilkka Piiroinen <ilkka.piiroinen@gmail.com>
2016-12-29 09:25:44 +0200

    div added

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,9 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-      <h3>Examples component</h3>
+      <div>
+        <h3>Examples component</h3>
+      </div>
     )
   }
 
