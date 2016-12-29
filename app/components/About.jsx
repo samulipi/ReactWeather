d362939d8fe881e64ea51335da681aca6487d752
Ilkka Piiroinen <ilkka.piiroinen@gmail.com>
2016-12-29 17:00:40 +0200

    style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,14 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-        <h3>About</h3>
-        <p>Welcome to the about page!</p>
-      </div>
+        <h1 className="text-center">About</h1>
+          <div className="success callout" data-closable>
+            <h5>This is a callout.</h5>
+            <p>It has an easy to override visual style, and is appropriately subdued.</p>
+            <a href="#">It's dangerous to go alone, take this.</a>
+          </div>
+
+        </div>
   )
 };
 
